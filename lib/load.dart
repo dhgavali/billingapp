ffc4011fdad7b0e603320a53898bde4b76e2e23f
dh <dhgavali@gmail.com>
2022-09-10 23:40:21 +0530

    created repo

---
+++ lib/load.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Load extends StatelessWidget {
+  const Load({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Consts {
+  // colors
+
+  static Color kprimary = const Color(0xFFFD4900);
+  static Color kdark = const Color(0xFFFFC500);
+}
